158c78f370a8fdc79a8992aebd4bcb08fc58a7df
kaushikkasodariya04 <priteshpatel@sourcecube.in>
2024-11-08 11:49:43 +0530

    minor-change

--- config/connect.js
+++ config/connect.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URL,
-        );
-        console.log("database connected successfully..");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-    }
-}
+  console.log("process.env.MONGO_URL", process.env.MONGO_URL);
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("database connected successfully..");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
